6ff7bdb66f3a558a598b7bb5e9dfeafb02a7e154
nikhita3005-lab <nikhita3005@gmail.com>
2026-05-14 22:22:47 +0530

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -19,3 +19,13 @@ CREATE TABLE Users(
         U_balance INT NOT NULL,
         CONSTRAINT U_balance_non_negative CHECK (U_balance >= 0)
     );
+CREATE TABLE Stores(
+        S_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
+        S_name VARCHAR(256) UNIQUE NOT NULL,
+        S_latitude FLOAT NOT NULL,
+        S_longitude FLOAT NOT NULL,
+        S_phone VARCHAR(10) NOT NULL,
+        S_foodtype VARCHAR(256) NOT NULL,
+        S_owner INT NOT NULL,
+        FOREIGN key (S_owner) REFERENCES Users(UID)
+    );
